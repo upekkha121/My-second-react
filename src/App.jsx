cda11966c6b63b6481eb4793d3a1d68f171de41e
upekkha <rajkarnikarupekkha02@gmail.com>
2025-01-13 14:59:52 +0545

    update on js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
-import Button from './components/buttons'
+import { useState } from 'react';
+import './App.css';
+// import Button from './components/buttons';
+import { sculptureList } from './js/data';
 
 function App() {
  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  let data = sculptureList[currentIndex];
+  console.log(data);
 
   return (
-    <>
-      <h2 className='text-[70px] text-center text-green-400 font-bold'>HelloWorld</h2>
-      <Button text={"Click me"}/>
-    </>
+    <div className='w-screen h-screen'>
+      {/* <h2 className='text-[70px] text-center text-green-400 font-bold'>HelloWorld</h2>
+      <Button text={"Click me"}/> */}
+      <div className='w-full h-full flex items-center justify-center'>
+        
+      </div>
+
+    </div>
   )
 }
 
